ab0970df870059e02f814cab9cae21b073f69bca
Lucca Lopes <110003708+luccalopes@users.noreply.github.com>
2026-01-21 18:45:36 -0300

    21/01/26

--- ex8.py
+++ ex8.py
@@ -2,30 +2,35 @@ import re
 import random
 import sys
 
-sys.exit()
-entry = input('CPF: ')
-nine_digits = re.sub(r'[^0-9]', '', entry)
-countdown_timer_1 = 10
+#sys.exit()
+# entry = input('CPF: ')
+# nine_digits = re.sub(r'[^0-9]', '', entry)
+for _ in range(100):
+    nine_digits = ''
+    for i in range(9):
+        nine_digits += str(random.randint(0, 9))
 
-digit1_result = 0
-for digit_1 in nine_digits:
-    digit1_result += int(digit_1) * countdown_timer_1
-    countdown_timer_1 -= 1 
-digit_1 = (digit1_result * 10) % 11
-digit_1 = digit_1 if digit_1 <= 9 else 0
-#print(digito_1)
+    countdown_timer_1 = 10
 
-ten_digits = nine_digits + str(digit_1)
-countdown_timer_2 = 11
+    digit1_result = 0
+    for digit_1 in nine_digits:
+        digit1_result += int(digit_1) * countdown_timer_1
+        countdown_timer_1 -= 1 
+    digit_1 = (digit1_result * 10) % 11
+    digit_1 = digit_1 if digit_1 <= 9 else 0
+    #print(digito_1)
 
-digit2_result = 0
-for digit_2 in ten_digits:
-    digit2_result += int(digit_2) * countdown_timer_2
-    countdown_timer_2 -= 1
-digit_2 = (digit2_result * 10) % 11
-digit_2 = digit_2 if digit_2 <= 9 else 0
-#print(digito_2)
+    ten_digits = nine_digits + str(digit_1)
+    countdown_timer_2 = 11
 
-new_cpf = f'{nine_digits}{digit_1}{digit_2}'
+    digit2_result = 0
+    for digit_2 in ten_digits:
+        digit2_result += int(digit_2) * countdown_timer_2
+        countdown_timer_2 -= 1
+    digit_2 = (digit2_result * 10) % 11
+    digit_2 = digit_2 if digit_2 <= 9 else 0
+    #print(digito_2)
 
-print(new_cpf)
+    new_cpf = f'{nine_digits}{digit_1}{digit_2}'
+
+    print(new_cpf)
